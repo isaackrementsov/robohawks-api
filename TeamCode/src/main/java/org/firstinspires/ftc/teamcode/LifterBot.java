6862b355295a25574e1413842ba0f282880d7e16
Isaac Krementsov <isaackrementsov@gmail.com>
2019-02-18 17:20:23 -0500

    Work on servos & encoded motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LifterBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LifterBot.java
@@ -4,9 +4,12 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 /**
  * Created by Sam on 11/2/2017. Copied and pasted by Willem. Viewed by Milo.
- * Added to by Isaac on 2/11/2019.
+ * Added to by Isaac (and Wolfie for algorithms) on 2/11/2019.
  */
 @TeleOp
 //Using inheritance to extend other methods
@@ -15,10 +18,11 @@ public class LifterBot extends OpMode {
     DcMotor motorRightB;
     DcMotor motorLeftA;
     DcMotor motorLeftB;
-    /*DcMotor spinner;
+    DcMotor lifter;
+    DcMotor spinner;
     DcMotor inOut;
-    Servo liftRight;
-    Servo liftLeft;*/
+    Servo bucketRight;
+    Servo bucketLeft;
     Servo grabberRight;
     Servo grabberLeft;
     public void init() { // initiates and maps motors/servos/sensors
@@ -26,13 +30,14 @@ public class LifterBot extends OpMode {
         motorRightB = hardwareMap.dcMotor.get("mRB");               // !! Must be prenamed in phone app to green letters (mRF, mRB, etc.) !!
         motorLeftA = hardwareMap.dcMotor.get("mLF");
         motorLeftB = hardwareMap.dcMotor.get("mLB");
-        /*inOut = hardwareMap.dcMotor.get("inOut");
+        inOut = hardwareMap.dcMotor.get("inOut");
         spinner = hardwareMap.dcMotor.get("spinner");
-        liftRight = hardwareMap.servo.get("liftR");
-        liftLeft = hardwareMap.servo.get("liftL");*/
+        bucketRight = hardwareMap.servo.get("bucketR");
+        bucketLeft = hardwareMap.servo.get("bucketL");
         grabberRight = hardwareMap.servo.get("grabberR");
         grabberLeft = hardwareMap.servo.get("grabberL");
-        //inOut.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        inOut.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        lifter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         //motorRightA.setDirection(DcMotor.Direction.REVERSE);      //think about logic of motors and how you need to reverse two of them
         //motorRightB.setDirection(DcMotor.Direction.REVERSE);
     }
@@ -49,8 +54,12 @@ public class LifterBot extends OpMode {
                 Left - strafe
                 Dpad - Maybe remove
         */
-        loopController1();
-        loopController2();
+        if(!gamepad1.atRest()){ //Make sure that a button is pressed before looping for performance reasons
+            loopController1();
+        }
+        if(!gamepad2.atRest()){
+            loopController2();
+        }
     }
     //Revised based on https://ftcforum.usfirst.org/forum/ftc-technology/android-studio/6361-mecanum-wheels-drive-code-example
     private void loopController1(){
@@ -83,8 +92,8 @@ public class LifterBot extends OpMode {
         telemetry.addData("Joy2", "Joystick 2:  " + String.format("%.2s", gamepad1.right_stick_y));
     }
     private void loopController2(){
-        double rightY = -gamepad2.right_stick_y; //Controls grabber/spinner arm, not continuous
-        double leftY = -gamepad2.left_stick_y; // Controls lifter arm, continuous
+        double rightY = -gamepad2.right_stick_y; //Controls lifter arm, continuous
+        double leftY = -gamepad2.left_stick_y; //Controls grabber/spinner arm, not continuous
         //This is boolean because it is checking whether button is pressed
         boolean a = gamepad2.a; //Turns on spinner
         boolean b = gamepad2.b; //Turns on spinner in reverse
@@ -92,18 +101,29 @@ public class LifterBot extends OpMode {
         boolean leftBumper = gamepad2.left_bumper; //button to dump
         rightY = Range.clip(rightY, -1, 1);
         leftY = Range.clip(leftY, -1, 1);
-        if(Math.abs(rightY) > .1){ //Threshold to prevent response to controller bumps
+        if(Math.abs(leftY) > .1){ //Threshold to prevent response to controller bumps
             double position = Range.clip((leftY + 1)/2, 0.14, 1); //So that position is not negative and between 0 and 1
             double position2 = Range.clip((-leftY + 1)/2, 0.14, 1);
             grabberRight.setPosition(position);
             grabberLeft.setPosition(position2);
+            telemetry.addData("Right servo position:", position);
+            telemetry.addData("Left servo position", position);
         }
-        /*if(Math.abs(leftY) > .1){
-            double position = Range.clip((leftY + 1)/2, 0.14, 1); //So that position is not negative and between 0 and 1
-            double position2 = Range.clip((-leftY + 1)/2, 0.14, 1); //Opposite position
-            lifterRight.setPosition(position); //No conditionals here because this is a continuous motion, possible establish limits later
-            lifterLeft.setPosition(position2);
-        }*/
+        if(Math.abs(rightY) > .1){
+            if(rightY > 0){
+                lifter.setTargetPosition(1);
+            }else if(rightY < 0){
+                lifter.setTargetPosition(-1);
+            }
+        }
+        if(rightBumper){
+            bucketRight.setPosition(1);
+            bucketLeft.setPosition(0);
+        }
+        if(leftBumper){
+            bucketRight.setPosition(0);
+            bucketLeft.setPosition(1);
+        }
         /*if(a){
             if(spinner.getPower() > 0){
                 spinner.setPower(0);
@@ -118,9 +138,5 @@ public class LifterBot extends OpMode {
                 spinner.setPower(-1);
             }
         }*/
-        if(rightBumper){
-            grabberRight.setPosition(1);
-            grabberLeft.setPosition(0);
-        }
     }
 }
