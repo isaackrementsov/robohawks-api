b8df5fa8a1370506b4ebf528a173ff6a26422a00
Isaac Krementsov <isaackrementsov@gmail.com>
2019-02-18 21:28:03 -0500

    Initial servo & encoded motor controllers added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LifterBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LifterBot.java
@@ -14,23 +14,30 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 @TeleOp
 //Using inheritance to extend other methods
 public class LifterBot extends OpMode {
-    DcMotor motorRightA;                    // creates motors in code
-    DcMotor motorRightB;
-    DcMotor motorLeftA;
-    DcMotor motorLeftB;
-    DcMotor lifter;
-    DcMotor spinner;
-    DcMotor inOut;
-    Servo bucketRight;
-    Servo bucketLeft;
-    Servo grabberRight;
-    Servo grabberLeft;
+    private boolean isDumping = false;
+    private boolean isReturning = false;
+    private boolean bucketReturning = false;
+    private boolean inOutDumping = false;
+    private boolean grabbersDumping = false;
+    private boolean lifterDumping = false;
+    private DcMotor motorRightA;                    // creates motors in code
+    private DcMotor motorRightB;
+    private DcMotor motorLeftA;
+    private DcMotor motorLeftB;
+    private DcMotor lifter;
+    private DcMotor spinner;
+    private DcMotor inOut;
+    private Servo bucketRight;
+    private Servo bucketLeft;
+    private Servo grabberRight;
+    private Servo grabberLeft;
     public void init() { // initiates and maps motors/servos/sensors
         motorRightA = hardwareMap.dcMotor.get("mRF");               // Finds the motor and the library to use it
         motorRightB = hardwareMap.dcMotor.get("mRB");               // !! Must be prenamed in phone app to green letters (mRF, mRB, etc.) !!
         motorLeftA = hardwareMap.dcMotor.get("mLF");
         motorLeftB = hardwareMap.dcMotor.get("mLB");
         inOut = hardwareMap.dcMotor.get("inOut");
+        lifter = hardwareMap.dcMotor.get("lifter");
         spinner = hardwareMap.dcMotor.get("spinner");
         bucketRight = hardwareMap.servo.get("bucketR");
         bucketLeft = hardwareMap.servo.get("bucketL");
@@ -43,7 +50,8 @@ public class LifterBot extends OpMode {
     }
     int speed = 1;
     @Override
-    public void loop() {                                        // goes into loop after the setup is done  in above void
+    public void loop() {
+        // goes into loop after the setup is done  in above void
         // sets up a loop for driving the robot
         /*
             TODO:
@@ -67,6 +75,8 @@ public class LifterBot extends OpMode {
         double rightX = gamepad1.right_stick_x; //Strafe
         double rightY = gamepad1.right_stick_y; //Speed
         double leftX = gamepad1.left_stick_x; //Yaw (Turn)
+        boolean rightBumper = gamepad1.right_bumper; //Make spinner arm go out
+        boolean leftBumper = gamepad1.left_bumper; //Make spinner arm go in
         rightX = Range.clip(rightX, -1, 1);
         rightY = Range.clip(rightY, -1, 1);// sets a value check to make sure we don't go over the desired speed (related to joysticks)
         leftX = Range.clip(leftX, -1, 1);
@@ -77,36 +87,50 @@ public class LifterBot extends OpMode {
         else if (gamepad1.dpad_down) speed = 2;
         else if (gamepad1.dpad_left) speed = 1;
 
-        if (Math.abs(rightX) > .1 || Math.abs(leftX) > .1 || Math.abs(rightY) > .1) {
+        if (Math.abs(rightX) > .1 || Math.abs(leftX) > .1 || Math.abs(rightY) > .1) { //Remember that left motor directions are reversed
+            //Speed always positive, strafing wheels must be opposed, speed has to be opposite on left and right
             motorRightA.setPower((rightY + rightX + leftX) / 4 * speed);
             motorRightB.setPower((rightY - rightX + leftX) / 4 * speed);
-            motorLeftA.setPower((-rightY + rightX + leftX) / 4 * speed);
+            motorLeftA.setPower((-rightY + rightX + leftX) / 4 * speed); //Opposite (negative) of right because the motors are opposite
             motorLeftB.setPower((-rightY - rightX + leftX) / 4 * speed);
+            //Strafe is not negative version because to strafe, on side goes out (<- ->), one goes in (-> <-)
+            //Forward       Turn        Strafe
+            /*  -> ->       -> ->       -> <-
+            *   -> ->       <- <-       <- ->
+            *  Think about force vectors created by 45° rotation on mecanum wheels to understand strafing
+            * */
         } else { //will not move if joysticks are not moving
             motorRightA.setPower(0);
             motorRightB.setPower(0);
             motorLeftA.setPower(0);
             motorLeftB.setPower(0);
         }
+        if(rightBumper){
+            inOut.setTargetPosition(1);
+        }else if(leftBumper){
+            inOut.setTargetPosition(0);
+        }
         telemetry.addData("Joy1", "Joystick 1:  " + String.format("%.2s", gamepad1.left_stick_y)); // feedback given to the driver phone from the robot phone
         telemetry.addData("Joy2", "Joystick 2:  " + String.format("%.2s", gamepad1.right_stick_y));
     }
     private void loopController2(){
-        double rightY = -gamepad2.right_stick_y; //Controls lifter arm, continuous
-        double leftY = -gamepad2.left_stick_y; //Controls grabber/spinner arm, not continuous
+        double rightY = gamepad2.right_stick_y; //Controls lifter arm, continuous
+        double leftY = gamepad2.left_stick_y; //Controls grabber/spinner arm, not continuous
+        float rightTrigger = gamepad2.right_trigger; //Turns on spinner
+        float leftTrigger = gamepad2.left_trigger; //Turns on spinner in reverse
         //This is boolean because it is checking whether button is pressed
-        boolean a = gamepad2.a; //Turns on spinner
-        boolean b = gamepad2.b; //Turns on spinner in reverse
-        boolean rightBumper = gamepad2.right_bumper; //button to reverse dump
-        boolean leftBumper = gamepad2.left_bumper; //button to dump
+        boolean rightBumper = gamepad2.right_bumper; //Move bucket down
+        boolean leftBumper = gamepad2.left_bumper; //Move bucket up
+        boolean dpadUp = gamepad1.dpad_up; //Bring arm and stuff out from dump
+        boolean dpadDown = gamepad1.dpad_down; //Bring arm and stuff in for dump
         rightY = Range.clip(rightY, -1, 1);
         leftY = Range.clip(leftY, -1, 1);
         if(Math.abs(leftY) > .1){ //Threshold to prevent response to controller bumps
             double position = Range.clip((leftY + 1)/2, 0.14, 1); //So that position is not negative and between 0 and 1
-            double position2 = Range.clip((-leftY + 1)/2, 0.14, 1);
+            double position2 = Range.clip((-leftY + 1)/2, 0, 0.86); //Opposing servos must have opposite directions
             grabberRight.setPosition(position);
             grabberLeft.setPosition(position2);
-            telemetry.addData("Right servo position:", position);
+            telemetry.addData("Right servo position:", position); //For debugging, can be removed later
             telemetry.addData("Left servo position", position);
         }
         if(Math.abs(rightY) > .1){
@@ -117,26 +141,65 @@ public class LifterBot extends OpMode {
             }
         }
         if(rightBumper){
+            bucketRight.setPosition(1);
+            bucketLeft.setPosition(0);
+        }else if(leftBumper){
             bucketRight.setPosition(0.22); //Minimum position is 45 degrees, so this goes to 40
             bucketLeft.setPosition(0.77);
         }
-        if(leftBumper){
-            bucketRight.setPosition(0);
-            bucketLeft.setPosition(1);
-        }
-        /*if(a){
+        if(rightTrigger > 0.1){
             if(spinner.getPower() > 0){
                 spinner.setPower(0);
             }else{
                 spinner.setPower(1);
             }
-        }
-        if(b){
+        }else if(leftTrigger > 0.1) {
             if(spinner.getPower() > 0){
                 spinner.setPower(0);
             }else{
                 spinner.setPower(-1);
             }
-        }*/
+        }
+        if(dpadUp || isReturning){
+            isReturning = true;
+            if(!bucketReturning){ //Make sure not to continuously ask buckets to move
+                bucketRight.setPosition(0.22); //return from dump
+                bucketLeft.setPosition(0.77);
+                bucketReturning = true;
+            }else if(bucketRight.getPosition() < 0.29){ //Wait until bucket is close to correct position
+                lifter.setTargetPosition(0);
+                grabberRight.setPosition(1);
+                grabberLeft.setPosition(0);
+                inOut.setTargetPosition(1);
+                spinner.setPower(1);
+                bucketReturning = false;
+                isReturning = false;
+            }
+        }else if(dpadDown || isDumping){
+            isDumping = true;
+            spinner.setPower(0); //Turn off and pull in spinner, dump, lift, dump
+            if(!inOutDumping){ //Don't keep resetting target position while it is already set
+                inOut.setTargetPosition(0);
+                inOutDumping = true;
+            }else if(!inOut.isBusy()){ //Wait for position to be reached
+                if(!grabbersDumping){
+                    grabberRight.setPosition(0.14);
+                    grabberLeft.setPosition(0.86);
+                    grabbersDumping = true;
+                }else if(grabberRight.getPosition() < 0.18){
+                    if(!lifterDumping){
+                        lifter.setTargetPosition(1);
+                        lifterDumping = true;
+                    }else if(!lifter.isBusy()){
+                        bucketRight.setPosition(1);
+                        bucketLeft.setPosition(0);
+                        isDumping = false;
+                        inOutDumping = false;
+                        grabbersDumping = false;
+                        lifterDumping = false;
+                    }
+                }
+            }
+        }
     }
 }
